73677ef914e07cc755a9f4862e135f1ebe55abcc
Aharon Seidman <aharondov1@gmail.com>
2024-05-05 01:12:48 -0400

    fixed bug with non optional shipper field on order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
   belongs_to :product
   belongs_to :customer, class_name: "User"
   belongs_to :address
-  belongs_to :shipper, class_name: "User"
+  belongs_to :shipper, class_name: "User", optional: true 
 end
