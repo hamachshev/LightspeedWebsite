d13fdc18f16112e3b9b3a62abb550d5b60574f74
Aharon Seidman <aharondov1@gmail.com>
2024-05-05 20:41:22 -0400

    trying again

--- config/environment.rb
+++ config/environment.rb
@@ -5,19 +5,19 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-File.open("./pushNotifications/key.p8", "w") do |f|
+File.open("pushNotifications/key.p8", "w") do |f|
 
   f.write(Rails.application.credentials.dig(:apple,:push_notification_key))
 
 end
 
 # Set read permissions on a file
-File.chmod(666, './pushNotifications/key.p8')
+File.chmod(666, 'pushNotifications/key.p8')
 
 
 APNS_CONNECTION = Apnotic::Connection.new(
   auth_method: :token,
-  cert_path: "./pushNotifications/key.p8" ,
+  cert_path: "pushNotifications/key.p8" ,
   key_id: "3NRUMUU6WC",
   team_id: "6QJ83977FH"
 )
