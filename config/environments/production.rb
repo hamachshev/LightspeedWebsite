490302998458b62e08500274619b0d4589219504
Aharon Seidman <aharondov1@gmail.com>
2024-09-03 21:18:23 -0400

    this worked b4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "lightspeed-fpe4g4dmfmapd3df.eastus-01.azurewebsites.net"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
